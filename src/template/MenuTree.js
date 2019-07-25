b5161ec876f133c2739b8cbe4074b0423ab7f1a7
schnr <44035863+schnr@users.noreply.github.com>
2019-07-25 09:04:38 -0300

    Update MenuTree.js

--- src/template/MenuTree.js
+++ src/template/MenuTree.js
@@ -5,9 +5,9 @@ export default class MenuTree extends Component {
         return (
                 <li className="treeview">
                     <a href={this.props.path}> 
-                        <i className={`fa fa-${this.props.icon}`}></i> 
+                        <i className={`fa fa-${this.props.icon}`} /> 
                         <span>{this.props.label}</span>
-                        <i className='fa fa-angle-left pull-right'></i>
+                        <i className='fa fa-angle-left pull-right' />
                     </a>
                     <ul className='treeview-menu'> 
                         {this.props.children}
